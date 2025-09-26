83b78b3465b19417597d549f10076657f178bdae
neirac <cneira@edgecast.io>
2025-09-26 23:54:01 +0000

    unit tests for presigned url

---
+++ test/s3-presigned-urls.test.js
@@ -0,0 +1,68 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+/*
+ * Copyright 2025 Edgecast Cloud LLC.
+ */
+
+///--- S3 Presigned URL Utility Tests
+// Using native nodeunit exports to avoid s3-test-helper module.parent issues
+
+exports['S3 presigned URL parameter detection'] = function (t) {
+    // Test basic parameter detection logic
+    var s3PresignedParams = {
+        'X-Amz-Algorithm': 'AWS4-HMAC-SHA256',
+        'X-Amz-Credential': 'AKIATEST12345/20250926/us-east-1/s3/aws4_request',
+        'X-Amz-Date': '20250926T120000Z',
+        'X-Amz-Expires': '3600',
+        'X-Amz-SignedHeaders': 'host',
+        'X-Amz-Signature': 'abcdef123456789'
+    };
+    
+    // Test detection function
+    function isS3PresignedURL(query) {
+        return query['X-Amz-Algorithm'] && 
+               query['X-Amz-Signature'] && 
+               query['X-Amz-Credential'];
+    }
+    
+    t.ok(isS3PresignedURL(s3PresignedParams), 'should detect S3 presigned URL');
+    t.ok(!isS3PresignedURL({}), 'should not detect empty query');
+    t.ok(!isS3PresignedURL({someParam: 'value'}), 'should not detect regular query');
+    
+    t.done();
+};
+
+exports['S3 credential parsing'] = function (t) {
+    // Test credential string parsing
+    var credential = 'AKIATEST12345/20250926/us-east-1/s3/aws4_request';
+    var parts = credential.split('/');
+    
+    t.equal(parts[0], 'AKIATEST12345', 'should extract access key');
+    t.equal(parts[1], '20250926', 'should extract date');
+    t.equal(parts[2], 'us-east-1', 'should extract region');
+    t.equal(parts[3], 's3', 'should extract service');
+    t.equal(parts[4], 'aws4_request', 'should extract request type');
+    
+    t.done();
+};
+
+exports['S3 date format validation'] = function (t) {
+    // Test ISO8601 date format: YYYYMMDDTHHMMSSZ
+    var validDate = '20250926T120000Z';
+    var invalidDate = '2025-09-26 12:00:00';
+    
+    function isValidS3Date(dateStr) {
+        return /^\d{8}T\d{6}Z$/.test(dateStr);
+    }
+    
+    t.ok(isValidS3Date(validDate), 'should validate correct S3 date format');
+    t.ok(!isValidS3Date(invalidDate), 'should reject invalid date format');
+    t.ok(!isValidS3Date(''), 'should reject empty date');
+    
+    t.done();
+};
+

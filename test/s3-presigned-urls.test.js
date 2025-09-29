5b5378f323fdcd90e190287bc8979da5f0917a1d
neirac <cneira@edgecast.io>
2025-09-29 19:22:04 +0000

    js lint s3-presigned-url tests

--- test/s3-presigned-urls.test.js
+++ test/s3-presigned-urls.test.js
@@ -21,18 +21,19 @@ exports['S3 presigned URL parameter detection'] = function (t) {
         'X-Amz-SignedHeaders': 'host',
         'X-Amz-Signature': 'abcdef123456789'
     };
-    
+
     // Test detection function
     function isS3PresignedURL(query) {
-        return query['X-Amz-Algorithm'] && 
-               query['X-Amz-Signature'] && 
+        return query['X-Amz-Algorithm'] &&
+               query['X-Amz-Signature'] &&
                query['X-Amz-Credential'];
     }
-    
+
     t.ok(isS3PresignedURL(s3PresignedParams), 'should detect S3 presigned URL');
     t.ok(!isS3PresignedURL({}), 'should not detect empty query');
-    t.ok(!isS3PresignedURL({someParam: 'value'}), 'should not detect regular query');
-    
+    t.ok(!isS3PresignedURL({someParam: 'value'}),
+         'should not detect regular query');
+
     t.done();
 };
 
@@ -40,13 +41,13 @@ exports['S3 credential parsing'] = function (t) {
     // Test credential string parsing
     var credential = 'AKIATEST12345/20250926/us-east-1/s3/aws4_request';
     var parts = credential.split('/');
-    
+
     t.equal(parts[0], 'AKIATEST12345', 'should extract access key');
     t.equal(parts[1], '20250926', 'should extract date');
     t.equal(parts[2], 'us-east-1', 'should extract region');
     t.equal(parts[3], 's3', 'should extract service');
     t.equal(parts[4], 'aws4_request', 'should extract request type');
-    
+
     t.done();
 };
 
@@ -54,15 +55,14 @@ exports['S3 date format validation'] = function (t) {
     // Test ISO8601 date format: YYYYMMDDTHHMMSSZ
     var validDate = '20250926T120000Z';
     var invalidDate = '2025-09-26 12:00:00';
-    
+
     function isValidS3Date(dateStr) {
-        return /^\d{8}T\d{6}Z$/.test(dateStr);
+        return (/^\d{8}T\d{6}Z$/.test(dateStr));
     }
-    
+
     t.ok(isValidS3Date(validDate), 'should validate correct S3 date format');
     t.ok(!isValidS3Date(invalidDate), 'should reject invalid date format');
     t.ok(!isValidS3Date(''), 'should reject empty date');
-    
+
     t.done();
 };
-
